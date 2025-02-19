79030330a1531442e570592e21863ccc92f09c15
Pascal Burri <pascal_burri@gmx.ch>
2025-02-19 09:38:37 +0100

    remove all and add plain html

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {HomeComponent} from "./components/home/home.component";
-import {AboutUsComponent} from "./components/about-us/about-us.component";
-
-export const routes: Routes = [
-  {path: "", pathMatch: "full", redirectTo: "home"},
-  {path: "home", component: HomeComponent},
-  {path: "about-us", component: AboutUsComponent},
-];
